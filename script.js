7541b9cca8bb3f12835b4d0cb313178de16102c0
EnzoFerigolo <enzoferigolo@MacBook-Air-de-Enzo.local>
2023-05-31 11:43:30 -0300

    javascript adicionado

---
+++ script.js
@@ -0,0 +1,31 @@
+// Hover no menu ativo
+
+const links = document.querySelectorAll(".links-menu a");
+
+links.forEach((link) => {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+});
+
+// Lista
+
+const itemLista = document.querySelectorAll(".lista button");
+
+itemLista.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    const controls = event.currentTarget.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+
+    event.currentTarget.setAttribute(
+      "aria-expanded",
+      resposta.classList.contains("ativa")
+    );
+    console.log(controls);
+  });
+});
